ffb2b1f05fa50f0a329acf67447c7fee4a9303dd
suwanie <swansea2013@naver.com>
2022-12-05 21:24:18 +0900

    v4 끝..!

--- src/main/java/hello/servlet/web/frontCtrl/MyView.java
+++ src/main/java/hello/servlet/web/frontCtrl/MyView.java
@@ -1,10 +1,12 @@
 package hello.servlet.web.frontCtrl;
 
+import com.fasterxml.jackson.annotation.JacksonInject.Value;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 public class MyView {
 
@@ -21,5 +23,22 @@ public class MyView {
     throws ServletException, IOException {
     RequestDispatcher dis = request.getRequestDispatcher(viewPath);
     dis.forward(request, response);
+  }
+
+  public void render(
+    Map<String, Object> model,
+    HttpServletRequest request,
+    HttpServletResponse response
+  ) throws ServletException, IOException {
+    modelToRequestAttribute(model, request);
+    RequestDispatcher dis = request.getRequestDispatcher(viewPath);
+    dis.forward(request, response);
+  }
+
+  private void modelToRequestAttribute(
+    Map<String, Object> model,
+    HttpServletRequest request
+  ) {
+    model.forEach((key, value) -> request.setAttribute(key, value));
   }
 }

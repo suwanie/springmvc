5b5affa3dd46cd44fd30067503156e678aec55f9
suwanie <swansea2013@naver.com>
2022-12-06 18:42:39 +0900

    21분15초까지 한게 핸들러 조회뿐?!

--- src/main/java/hello/servlet/web/frontCtrl/v5/FrontCtrlServletV5.java
+++ src/main/java/hello/servlet/web/frontCtrl/v5/FrontCtrlServletV5.java
@@ -1,10 +1,18 @@
 package hello.servlet.web.frontCtrl.v5;
 
+import hello.servlet.web.frontCtrl.ModelView;
+import hello.servlet.web.frontCtrl.MyView;
+import hello.servlet.web.frontCtrl.v3.ControllerV3;
 import hello.servlet.web.frontCtrl.v3.controller.MemberFormControllerV3;
 import hello.servlet.web.frontCtrl.v3.controller.MemberListControllerV3;
 import hello.servlet.web.frontCtrl.v3.controller.MemberSaveControllerV3;
+import hello.servlet.web.frontCtrl.v5.adapter.ControllerV3HandlerAdapter;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -20,6 +28,12 @@ public class FrontCtrlServletV5 extends HttpServlet {
   private final List<MyHandlerAdapter> handlerAdapters = new ArrayList<>();
 
   public FrontCtrlServletV5() {
+    initHandlerMappingMap(); // public이면 왜 접근을 못하지?
+
+    initHandlerAdapters();
+  }
+
+  private void initHandlerMappingMap() {
     handlerMappingMap.put(
       "/front-controller/v3/members/new-form",
       new MemberFormControllerV3()
@@ -32,5 +46,44 @@ public class FrontCtrlServletV5 extends HttpServlet {
       "/front-controller/v3/members",
       new MemberListControllerV3()
     );
+  }
+
+  private void initHandlerAdapters() {
+    handlerAdapters.add(new ControllerV3HandlerAdapter());
+  }
+
+  @Override
+  protected void service(
+    HttpServletRequest request,
+    HttpServletResponse response
+  ) throws ServletException, IOException {
+    Object handler = getHandler(request);
+
+    if (handler == null) {
+      response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+      return;
+    }
+
+    Map<String, String> paramMap = createParamMap(request);
+    ModelView mv = controller.process(paramMap);
+
+    String viewName = mv.getViewName();
+    MyView view = viewResolver(viewName);
+
+    view.render(mv.getModel(), request, response);
+  }
+
+  private MyView viewResolver(String viewName) {
+    return null;
+  }
+
+  private Map<String, String> createParamMap(HttpServletRequest request) {
+    return null;
+  }
+
+  private Object getHandler(HttpServletRequest request) {
+    String requestURI = request.getRequestURI();
+
+    return handlerMappingMap.get(requestURI);
   }
 }
